06da5f6b90e1a76a910081e225d0de85cdb11d53
TECH-A52 <tech-a52@TECH-A52s-MacBook-Air.local>
2018-06-21 09:29:33 +0100

    DataBase and relations complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Country.create(name:Faker::Address.country)
+end
+
+15.times do
+  country = Country.all.length
+  Author.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age:Faker::Number.between(20, 50), country_id: rand(1..country))
+end
+
+20.times do
+  author = Author.all.length
+  Book.create(title: Faker::Book.title, genre: Faker::Book.genre, published:Faker::Date.backward(14), author_id: rand(1..author))
+end
+
+# At this point do something like
+# Book.find(18).author.country.authors.all.first.books
+# just to blow their minds
+
+# Create 25 Random Chapters
+25.times do
+  book = Book.all.length
+  Chapter.create(title: Faker::ChuckNorris.fact, book_id: rand(1..book))
+end
+
+30.times do
+  chapter = Chapter.all.length
+  Paragraph.create(body: Faker::Lorem.sentences(3), chapter_id: rand(1..chapter))
+end
